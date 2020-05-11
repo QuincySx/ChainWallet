4ebe701cef3246a941c6ded1519a2a42f2727dab
QuincySx <772804430@qq.com>
2020-05-11 11:59:32 +0800

    test c++ package size

--- app/src/main/java/com/smallraw/chain/wallet/MainActivity.kt
+++ app/src/main/java/com/smallraw/chain/wallet/MainActivity.kt
@@ -2,11 +2,14 @@ package com.smallraw.chain.wallet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.smallraw.chain.lib.Testss
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Testss.test()
     }
 }
---
+++ lib-chain-bitcoin/src/main/java/com/smallraw/chain/lib/Testss.java
@@ -0,0 +1,9 @@
+package com.smallraw.chain.lib;
+
+import com.smallraw.chain.lib.crypto.Secp256K1;
+
+public class Testss {
+    public static void test() {
+        Secp256K1.INSTANCE.createPrivateKey();
+    }
+}
